84c17504c17a194d1c315465d683e46cceb1e241
Svenn-andre Alvestad <svennand@gmail.com>
2018-09-30 21:03:36 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Svenn-Andre Alvestad on 30/09/2018.
+//  Copyright © 2018 Svenn-Andre Alvestad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
